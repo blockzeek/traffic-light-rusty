03a51095847c0702d38bf3e93baca911af3bac04
blockzeek <100653223+blockzeek@users.noreply.github.com>
2022-07-12 18:56:50 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,16 +1,30 @@
 enum TrafficLight {
-	Red,
-	Green,
-	Yellow,
+    Red,
+    Green,
+    Yellow,
 }
 
-impl TrafficLight {
-    fn duration(&self) -> u8{
-        60
+trait Duration {
+    fn duration(&self) -> u8;
+}
+
+impl Duration for TrafficLight {
+    fn duration(&self) -> u8 {
+        match &self {
+            TrafficLight::Red => 0,
+            TrafficLight::Green => 1,
+            TrafficLight::Yellow => 255,
+        }
     }
 }
 
 fn main() {
+    let red_light = TrafficLight::Red;
+    println!("red light is {}", red_light.duration());
+
+    let green_light = TrafficLight::Green;
+    println!("green light is {}", green_light.duration());
+
     let yellow_light = TrafficLight::Yellow;
     println!("yellow light is {}", yellow_light.duration());
 }
